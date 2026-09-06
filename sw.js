5ec10da557674df1681b19e92bba9eacf826df1b
Oleksandr Ivanchenko <alexsergivan@gmail.com>
2026-09-06 18:54:52 +0200

    Deploy Pit multiplayer fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'blink-tax-v17';
+const CACHE = 'blink-tax-v18';
 const SHELL = ['./index.html', './manifest.webmanifest', './icons/icon.svg'];
 const ASSET_PATTERN = /\.(?:js|css|png|jpe?g|gif|svg|webp|ico|webmanifest)(?:$|\?)/i;
 
