1f27f53ba32671b7b63bcfb7a2c940c32cf8760c
Oleksandr Ivanchenko <alexsergivan@gmail.com>
2026-09-06 16:18:37 +0200

    Ship camera-hide + stronger cartoon mugshot (SW v14)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'blink-tax-v13';
+const CACHE = 'blink-tax-v14';
 const SHELL = ['./index.html', './manifest.webmanifest', './icons/icon.svg'];
 const ASSET_PATTERN = /\.(?:js|css|png|jpe?g|gif|svg|webp|ico|webmanifest)(?:$|\?)/i;
 
